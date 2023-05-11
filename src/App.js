42692f7aaea98020eafd2f41060e94593260b001
Victor Koomson <vkoomson@persolgh.com>
2023-05-11 08:41:04 +0000

    updated menu-items to navigate withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import React from 'react';
+import { Route, Switch} from 'react-router-dom';
 import Homepage from './pages/homepage/homepage.component';
 import './App.css';
+
+const HatsPage = () => (
+  <div>
+    <h1>HAT'S PAGE</h1>
+  </div>
+)
+const JacketsPage = () => (
+  <div>
+    <h1>JACKET'S PAGE</h1>
+  </div>
+)
 
 function App() {
   return (
     <div>
-      <Homepage />
+      <Switch>
+        <Route exact path="/" component={Homepage} />
+        <Route path="/hats" component={HatsPage} />
+        <Route path="/jackets" component={JacketsPage} />
+      </Switch>
     </div>
   );
 }
