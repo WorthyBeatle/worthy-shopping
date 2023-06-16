2e79cb7566ec70b7a6fe153674bf12e6fdb8ecd8
Victor Koomson <vkoomson@persolgh.com>
2023-06-16 15:43:44 +0000

    finishing sign in in handle Submit method

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -11,8 +11,35 @@ const config = {
     appId: "1:59484658265:web:47df18a80139ae85bb6f48",
     measurementId: "G-RMKGP3KBWL"
   }
-
+  
   firebase.initializeApp(config);
+
+  export const createUserProfileDocument = async (userAuth, additionalData) => {
+    if(!userAuth) return;
+
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+    const snapShot = await userRef.get();
+
+    if (!snapShot.exists) {
+        const { displayName, email} = userAuth;
+        const createdAt = new Date();
+
+        try {
+            await userRef.set({
+                displayName,
+                email,
+                createdAt,
+                ...additionalData
+            })
+        } catch (error) {
+            console.log('error creating user', error.message);
+        }
+    }
+
+    return userRef;
+  }
+
 
   export const auth = firebase.auth();
   export const firestore = firebase.firestore();
